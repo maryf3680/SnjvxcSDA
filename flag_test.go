dc6d24b3e6e7a209981796ad417dcebd99ef0a16
maryf3680 <104142863+maryf3680@users.noreply.github.com>
2022-07-20 03:11:23 +1000

    Update flag_test.go

--- flag_test.go
+++ flag_test.go
@@ -9,13 +9,11 @@ import "testing"
 func TestDecodeArg(t *testing.T) {
 	t.Parallel()
 	tests := []struct {
-		arg, want string
 	}{
 		{"", ""},
 		{"hello", "hello"},
 		{"hello\\n", "hello\n"},
 		{"hello\\nthere", "hello\nthere"},
-		{"hello\\\\there", "hello\\there"},
 		{"\\\\\\n", "\\\n"},
 	}
 	for _, test := range tests {
